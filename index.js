2e2520efe138d7bb6cd13774b8cf91f301715b2e
Parishri Shah <136893851+Parishri07@users.noreply.github.com>
2023-09-07 17:48:06 +0000

    final changes

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var button = document.getElementById("dice");
-  
-    button.addEventListener("click", async function() {
-      let response = await getAPI();
-      document.querySelector(".adviceNumber").textContent = response.slip.id;
-      document.querySelector(".advice").textContent = response.slip.advice;
-    });
-  
-    const getAPI = async () => {
-      let p = await fetch("https://api.adviceslip.com/advice");
-      let response = await p.json();
-      return response;
-    };
+  var button = document.getElementById("dice");
+
+  button.addEventListener("click", async function() {
+    addGlowEffect();
+    let response = await getAPI();
+    document.querySelector(".adviceNumber").textContent = response.slip.id;
+    document.querySelector(".advice").textContent = response.slip.advice;
   });
+
+  const getAPI = async () => {
+    let p = await fetch("https://api.adviceslip.com/advice");
+    let response = await p.json();
+    return response;
+  };
+});
+
+function addGlowEffect() {
+  const diceButton = document.getElementById('dice');
+  diceButton.classList.add('glow');
   
+  setTimeout(() => {
+    diceButton.classList.remove('glow');
+  }, 950);
+}
